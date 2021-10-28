637315b6891ca26ca8bce069f4ebafb4ebf3bc3c
Pedro Ulises Ávila Núñez <minimainor_austin@hotmail.com>
2021-10-28 13:30:20 -0500

    escribir pruebas de confianza

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -22,12 +22,13 @@ namespace TestNinja.UnitTests
 
 
         [Test]
-        [Ignore("Because I wanted to!")]
+        //[Ignore("Because I wanted to!")]
         public void AddWhenCalled_ReturnTheSumOfArguments()
         {
             var math = new Math();
             var result = math.Add(1, 2);
             Assert.That(result, Is.EqualTo(3));
+            
         }
 
         [Test]
